96ea8b02aedec4000ff7d7d0d187ca32a28c0987
jzavorskas <92336939+jzavorskas@users.noreply.github.com>
2024-08-27 15:04:15 -0400

    Update Soybean_Master.py

--- Soybean_Master.py
+++ Soybean_Master.py
@@ -1,49 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Start Date: 2/02/2023
-Last Edit: 5/16/2023
-
-In this script, I will be using visualizing the SoyNAM soybean dataset 
-with the uniform manifold approximation and project (UMAP) algorithm.
-
-Then, without UMAP dimensionality reduction, a random forest (RF) will be trained to 
-predict a set of phenotypes from the genotype data available. A genetic 
-algorithm (GA) will then be used to try to produce novel phenotypes by using the
-trained RF as a forward simulator. The GA's solutions will be genotype with the
-same format as the SoyNAM set, and will progress until a genotype that provides
-the desired phenotype is discovered. The solution space of this problem is
-massive, and will likely require an explorative, incremental approach similar
-to the "Design, Build, Test, Learn" (DBTL) paradigm.
-
-"""
-# %% Table of Contents
-
-"""
-##########################################
-Part 1: Definitions and Visualization
-a) Import and Preprocessing
-b) UMAP Optimization by GA; Visualization of Phenotypes
-c) Random Forest Validation; K-Fold Cross Validation
-_______________________________________________
-Part 2: Problem Statement
-a) Maximum SNP Tolerance Definition; Find Genotypes with Neighbors
-b) Can GA/RF find a removed point which is known to have nearest neighbors?
-_______________________________________________
-Part 3: Visualization and "Sanity Check" of Pipeline
-a) Metrics by Bin: Generated Individual close to Genotypes of similar phenotype?
-b) UMAP Visualization of Generated Individuals against full population's phenotypes.
-c) UMAP Visualization of Generated Individuals against target neighbor cluster.
-_______________________________________________
-Part 4: Incremental Optimization Proof of Concept
-a) GA Restricted to just individuals in a cluster. Can we accurately make an incremental change?
-b) In the largest cluster (19 individuals), how many of them can be recreated (reasonable time limit)?
-_______________________________________________
-Part 5: Incremental Optimization Case Study
-a) Choose phenotype(s) to optimize.
-b) Loop incremental optimization, adding new individuals to the pipeline along the way.
-c) Visualize progression of individuals toward the phenotype goal.
-
-"""
 
 # %% Step 1a) Import necessary packages.
 import numpy as np
